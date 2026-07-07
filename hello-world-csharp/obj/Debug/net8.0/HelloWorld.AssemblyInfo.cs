c0cc08fd8fc9f6aea5b73611ce326d831b480fcc
Uwa Joseph <uwa@soar.com>
2026-07-07 17:11:44 +0100

    week 01 updated

--- hello-world-csharp/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ hello-world-csharp/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d52b9aac1d4d30497133624991772475637738")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872ecd49c003fddb131d62dd35f72a1b2783b0d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
